f9fbbd8af98e52fb3df5ec7203659ce8cbeb114a
devio-at <github@devio.at>
2020-03-09 13:15:33 +0100

    tsql

---
+++ 400 Synonyms.sql
@@ -0,0 +1,41 @@
+
+CREATE SYNONYM hcwBoardChannels FOR hcwole...hcwBoardChannels
+GO
+CREATE SYNONYM hcwBoards FOR hcwole...hcwBoards
+GO
+CREATE SYNONYM hcwChannels FOR hcwole...hcwChannels
+GO
+CREATE SYNONYM hcwDevices FOR hcwole...hcwDevices
+GO
+CREATE SYNONYM hcwDeviceSources FOR hcwole...hcwDeviceSources
+GO
+CREATE SYNONYM hcwEpgUpdates FOR hcwole...hcwEpgUpdates
+GO
+CREATE SYNONYM hcwPlaybackHistory FOR hcwole...hcwPlaybackHistory
+GO
+CREATE SYNONYM hcwRatingDimension FOR hcwole...hcwRatingDimension
+GO
+CREATE SYNONYM hcwRatingDimensionValues FOR hcwole...hcwRatingDimensionValues
+GO
+CREATE SYNONYM hcwRatingRegion FOR hcwole...hcwRatingRegion
+GO
+CREATE SYNONYM hcwRecurringRecording FOR hcwole...hcwRecurringRecording
+GO
+CREATE SYNONYM hcwSatelliteSources FOR hcwole...hcwSatelliteSources
+GO
+CREATE SYNONYM hcwScheduledRecording FOR hcwole...hcwScheduledRecording
+GO
+CREATE SYNONYM hcwServiceAnalogue FOR hcwole...hcwServiceAnalogue
+GO
+CREATE SYNONYM hcwServiceDigital FOR hcwole...hcwServiceDigital
+GO
+CREATE SYNONYM hcwServiceEvents FOR hcwole...hcwServiceEvents
+GO
+CREATE SYNONYM hcwServicePids FOR hcwole...hcwServicePids
+GO
+CREATE SYNONYM hcwServices FOR hcwole...hcwServices
+GO
+CREATE SYNONYM hcwSuites FOR hcwole...hcwSuites
+GO
+CREATE SYNONYM hcwSuiteSet FOR hcwole...hcwSuiteSet
+GO
